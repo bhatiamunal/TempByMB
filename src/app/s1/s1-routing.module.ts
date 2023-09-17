acaa6042259889686f2b1638fd1dd4d5bd16867a
munal bhatia <bhatiamunal8987@gmail.com>
2023-09-18 00:09:27 +0530

    card bootstrap 18 09 2023

--- src/app/s1/s1-routing.module.ts
+++ src/app/s1/s1-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { S1Component } from './s1.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import { CardComponent } from './card/card.component';
 const routes: Routes = [
   { path: '', component: S1Component },
   { path: 'AboutUs', component: AboutUsComponent },
   { path: 'ContactUs', component: ContactUsComponent },
+  { path: 'Card', component: CardComponent },
 ];
 
 @NgModule({
--- src/app/s1/s1.module.ts
+++ src/app/s1/s1.module.ts
@@ -5,13 +5,15 @@ import { S1RoutingModule } from './s1-routing.module';
 import { S1Component } from './s1.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import { CardComponent } from './card/card.component';
 
 
 @NgModule({
   declarations: [
     S1Component,
     AboutUsComponent,
-    ContactUsComponent
+    ContactUsComponent,
+    CardComponent
   ],
   imports: [
     CommonModule,
