8da7b18cfe927934953446e8676383a45e85e7bc
munal bhatia <bhatiamunal8987@gmail.com>
2023-10-02 10:30:49 +0530

    Route gaurd active

---
+++ src/app/auth-gaurd.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGaurdService {
+
+  constructor(
+    private router: Router,
+ 
+  ) { }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    
+    // if (user) {
+    //     // authorised so return true
+    //     return true;
+    // }
+
+    // // not logged in so redirect to login page with the return url
+    // this.router.navigate(['/account/login'], { queryParams: { returnUrl: state.url } });
+    return false;
+}
+}
