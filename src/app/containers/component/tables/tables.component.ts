2c274ca6eba860eb9e6bf5f6bb0c380b129ba3ed
munal bhatia <bhatiamunal8987@gmail.com>
2023-10-10 00:44:46 +0530

    table error dynamic

--- src/app/containers/component/tables/tables.component.ts
+++ src/app/containers/component/tables/tables.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./tables.component.css']
 })
 export class TablesComponent {
-
+  obj = [ { c0: "a", c1: "b", c2: "c" }, { c0: "e", c1: "f", c2: "g" } ]; 
+   KeyArr:String[]
+   access:any
+  constructor(){ 
+    this.KeyArr = Object.keys(this.obj[0]); 
+   
+    this.access = (str: keyof typeof this.KeyArr) => {
+      return this.obj[str];
+    };
+  }
+  getHeaders() {
+    let headers: string[] = [];
+    if(this.obj) {
+      this.obj.forEach((value) => {
+        Object.keys(value).forEach((key) => {
+          if(!headers.find((header) => header == key)){
+            headers.push(key)
+          }
+        })
+      })
+    }
+    return headers;
+  }
+  
 }
